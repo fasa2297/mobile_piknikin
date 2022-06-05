1a45a984be3eefbfd8c795400a29ebab34a8a914
fasa2297 <fasaalha@gmail.com>
2022-06-05 18:36:32 +0700

    Update management dart file

---
+++ lib/landingpage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  @override
+  _LandingPage createState() => _LandingPage();
+}
+
+class _LandingPage extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.black),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/Bg-landingpage.jpg"),
+                fit: BoxFit.cover),
+          ),
+          child: Center(
+            child: Text(
+              "Piknikin di Museum",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,20 @@
 //* page 1
-import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:mobile_piknikin/landingpage.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.black),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/Bg-landingpage.jpg"),
-                fit: BoxFit.cover),
-          ),
-          child: Center(
-            child: Text(
-              "Piknikin di Museum",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ),
-      ),
+      home: LandingPage(),
     );
   }
 }
-
+//*/
 
 /* Page 2
 import 'package:flutter/material.dart';
@@ -145,142 +127,17 @@ class MyApp extends StatelessWidget {
 
 /* Page 3
 import 'package:flutter/material.dart';
+import 'package:mobile_piknikin/pilihanmuseum.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    //final title = 'MyApp';
-
     return MaterialApp(
-      //title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color(0xfffffff),
-          title: Row(
-            children: [
-              Image.asset(
-                'assets/images/Logo.png',
-                fit: BoxFit.contain,
-                height: 32,
-              ),
-            ],
-          ),
-
-          /*title: Container(
-            width: 110,
-            child: Image.asset('assets/images/Logo.png'),
-          ),*/
-        ),
-        body: ListView(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.fromLTRB(10, 20, 5, 0),
-              /*kiri */
-              child: Text(
-                'Museum Sejarah',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(8.0),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(8.0))),
-                child: InkWell(
-                  onTap: () => print("ciao"),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch, // add this
-                    children: <Widget>[
-                      ClipRRect(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(8.0),
-                          topRight: Radius.circular(8.0),
-                        ),
-                        child: Image.asset(
-                            'assets/images/Monumen_jogja_kembali.jpg',
-                            // width: 300,
-                            height: 150,
-                            fit: BoxFit.fill),
-                      ),
-                      ListTile(
-                        title: Text('Monumen Jogja Kembali'),
-                        subtitle: Text(
-                            'Jl. Ring Road Utara, Jongkang, Sariharjo, Kec. Ngaglik, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55581'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(8.0),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(8.0))),
-                child: InkWell(
-                  onTap: () => print("ciao"),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      ClipRRect(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(8.0),
-                          topRight: Radius.circular(8.0),
-                        ),
-                        child: Image.asset(
-                            'assets/images/IndonesianAirForceMuseum.jpg',
-                            // width: 300,
-                            height: 150,
-                            fit: BoxFit.fill),
-                      ),
-                      ListTile(
-                        title: Text('Museum Pusat TNI AU Dirgantara Mandala'),
-                        subtitle: Text(
-                            'Kompleks Landasan Udara Adisucipto, Jl. Raya Janti, Banguntapan, Kec. Banguntapan, Kabupaten Bantul, Daerah Istimewa Yogyakarta 55282'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(8.0),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(8.0))),
-                child: InkWell(
-                  onTap: () => print("ciao"),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      ClipRRect(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(8.0),
-                          topRight: Radius.circular(8.0),
-                        ),
-                        child: Image.asset('assets/images/museum-jogja.jpg',
-                            // width: 300,
-                            height: 150,
-                            fit: BoxFit.fill),
-                      ),
-                      ListTile(
-                        title: Text('Museum HM Soeharto'),
-                        subtitle: Text(
-                            'Dusun, Jl. Kemusuk Lor Jl. Nulis-Puluhan, Srontakan, Argomulyo, Kec. Sedayu, Kabupaten Bantul, Daerah Istimewa Yogyakarta 55752'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: PilihanMuseum(),
     );
   }
 }
