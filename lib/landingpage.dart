6a644a3e23866f131109f1427a8e6b76e1d99d5f
fasa2297 <fasaalha@gmail.com>
2022-06-05 20:42:18 +0700

    Co-authored-by: akibbmuh <akibbmuh@users.noreply.github.com> Co-authored-by: intananindika <intananindika@users.noreply.github.com> Co-authored-by: Zahra Fadiah Putri <amboneeee@users.noreply.github.com>

--- lib/landingpage.dart
+++ lib/landingpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_piknikin/pilihanmuseum.dart';
 
 class LandingPage extends StatefulWidget {
   @override
@@ -9,9 +10,50 @@ class _LandingPage extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.black),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+        theme: ThemeData(primaryColor: Colors.black),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+              Container(
+                  margin: const EdgeInsets.all(5),
+                  child: Text(
+                    'Piknikin di Museum',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  )),
+              Container(
+                  margin: const EdgeInsets.all(5),
+                  child: Text(
+                    'Pecinta seni? piknikin aja..',
+                    style:
+                        TextStyle(fontSize: 24, fontWeight: FontWeight.normal),
+                  )),
+              Container(
+                  margin: const EdgeInsets.all(5),
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const PilihanMuseum()),
+                      );
+                    },
+                    label: Text('Ayo Eksplor Museumnya ->'),
+                    icon: Icon(Icons.favorite),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color.fromARGB(255, 139, 139, 139),
+                      textStyle: TextStyle(
+                        color: Color.fromARGB(255, 31, 31, 31),
+                        fontSize: 22,
+                      ),
+                    ),
+                  )),
+            ]))));
+  }
+}
+/*
         body: Container(
           decoration: BoxDecoration(
             image: DecorationImage(
@@ -19,13 +61,15 @@ class _LandingPage extends State<LandingPage> {
                 fit: BoxFit.cover),
           ),
           child: Center(
-            child: Text(
-              "Piknikin di Museum",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+            child: ElevatedButton(
+              child: const Text('Ayo eksplor Museumnya ->'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const PilihanMuseum()),
+                );
+              },
             ),
           ),
         ),
@@ -33,3 +77,11 @@ class _LandingPage extends State<LandingPage> {
     );
   }
 }
+            child: Text(
+              "Piknikin di Museum",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+),*/
