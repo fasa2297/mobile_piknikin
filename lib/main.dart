ac642edb86c0d712e536526ea8699830f3b5775a
fasa2297 <fasaalha@gmail.com>
2022-06-15 22:28:32 +0700

    Update connect to api

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 //import 'dart:convert';
-
+import 'package:mobile_piknikin/screens_backend/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_piknikin/landingpage.dart';
-//import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;
 
 //--------testing ambil data dari db
+
 void main() => runApp(MyApp());
 
+/*
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-/*
+*/
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -33,49 +35,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePage createState() => _HomePage();
-}
-
-class _HomePage extends State<HomePage> {
-  List museumlist = [];
-
-  getAllmuseum() async {
-    var respone = await http.get("http://127.0.0.1/php_run/DataMuseum.php");
-    if (respone.statusCode == 200) {
-      setState(() {
-        museumlist = json.decode(respone.body);
-      });
-      print(museumlist);
-      return museumlist;
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getAllmuseum();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('test ambil data'),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.add), onPressed: () {}),
-        ],
-      ),
-      body: ListView.builder(
-          itemCount: museumlist.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: Text(museumlist[index]['nama']),
-            );
-          }),
-    );
-  }
-}
-*/
