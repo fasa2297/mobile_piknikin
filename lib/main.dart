3d4d7490995b9919996df4b35603f172525a4304
fasa2297 <fasaalha@gmail.com>
2022-06-25 12:22:23 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
-//import 'dart:convert';
-import 'package:mobile_piknikin/screens_backend/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_piknikin/landingpage.dart';
-import 'package:http/http.dart' as http;
-
-//--------testing ambil data dari db
 
 void main() => runApp(MyApp());
 
-// ------------ Front end progress
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -20,20 +14,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// ----------- Back end progress
-/*
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ambil_data',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomePage(),
-    );
-  }
-}*/
